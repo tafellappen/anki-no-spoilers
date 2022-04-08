277e0f01556a97cb13f7a7e598586971c686fc6a
Miranda <mauriemma12@gmail.com>
2022-04-08 17:43:53 -0400

    got initial demo working - of editing card html and editing card fields

--- anki-no-spoilers/no_spoilers/__init__.py
+++ anki-no-spoilers/no_spoilers/__init__.py
@@ -1,23 +1,78 @@
 # import the main window object (mw) from aqt
 from aqt import mw
+from aqt import gui_hooks
 # import the "show info" tool from utils.py
 from aqt.utils import showInfo, qconnect
 # import all of the Qt GUI library
 from aqt.qt import *
+import re
 
-# We're going to add a menu item below. First we want to create a function to
-# be called when the menu item is activated.
+def beforeShowCard(html, card, context):
+    makeBlueScript = """
+<script>
+document.body.style.background = "blue";
+</script>"""
+    # answer = card.answer()
+    # if answer in html
 
-def testFunction() -> None:
-    # get the number of cards in the current collection, which is stored in
-    # the main window
-    cardCount = mw.col.cardCount()
-    # show a message box
-    showInfo("Card count: %d" % cardCount)
+    return html + makeBlueScript
+
+
+#########################
+def testModCards():
+    ids = mw.col.findCards("deck:プログラミング")
+    testingCap = 20
+    cardNumber = 0
+
+    #https://www.reddit.com/r/Anki/comments/5b1p7v/cant_edit_note_field_in_python/
+    for id in ids:
+        if (cardNumber >= testingCap):
+            break
+        card = mw.col.getCard(id)
+        note = card.note()
+        for (name, value) in note.items():
+
+            # if (name == "Spanish Word"):
+            note[name] = value + "something new"
+
+
+            # showInfo(value)
+
+        note.flush()
+        cardNumber += 1
+
+def removeSpoilers():
+    ids = mw.col.find_notes("deck:プログラミング")
+    for id in ids:
+        note = mw.col.getNote(id)
+        for (name, value) in note.items():
+            # showInfo(name)
+            if (name == "furigana-plain(expression)"):
+                wordToScreenFor = note[name]
+
+                fieldNameToScan = "Front of Card Sentence"
+                fieldToScan = note[fieldNameToScan]
+
+                # #remove furigana from the word, the sentence will not have it
+                furi = re.search("(\[.*\])", wordToScreenFor)
+                wordWithoutFuri = furi.group(1)
+
+                note[fieldNameToScan] = fieldToScan.replace(wordToScreenFor, "[|||no peeking|||]")
+
+
+
+
+        note.flush()
+    showInfo("Done")
 
 # create a new menu item, "test"
-action = QAction("test", mw)
+action = QAction("remove spoilers", mw)
 # set it to call testFunction when it's clicked
-qconnect(action.triggered, testFunction)
+qconnect(action.triggered, removeSpoilers)
 # and add it to the tools menu
 mw.form.menuTools.addAction(action)
+
+
+gui_hooks.card_will_show.append(beforeShowCard)
+
+
